70a69945849e0438a40875de271ccf6946a931de
Jesus <jesus.fdz93@gmail.com>
2023-07-22 14:18:54 -0500

    product

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import ProductContext from "../context/ProductContext";
+import { Link } from "react-router-dom";
 
 const ProductsPage = () => {
   const { getProducts, products } = useContext(ProductContext);
 
   const handleClick = async () => {
     await getProducts();
+  };
+
+  const handleAddProduct = (id) => {
+    console.log(id);
   };
 
   return (
@@ -29,6 +34,22 @@ const ProductsPage = () => {
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">{product.price}</p>
+
+                <div className="d-flex justify-content-between">
+                  <button
+                    type="button"
+                    className="btn btn-warning btn-lg"
+                    onClick={() => handleAddProduct(product.id)}
+                  >
+                    <i className="bi bi-cart-plus" />
+                  </button>
+                  <Link
+                    to={`/products/${product.id}`}
+                    className="btn btn-info btn-lg"
+                  >
+                    <i className="bi bi-box-seam" />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
