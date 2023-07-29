885e79073a7dc9de2c88b569da86949b73012ee6
Jesus <jesus.fdz93@gmail.com>
2023-07-29 15:04:06 -0500

    cambios

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const URL_ROOT = "https://dwfs16-m3s21-ejercicio-auth.onrender.com/api/auth";
-const URL_ROOT = "http://localhost:4000/api/auth";
+// const URL_ROOT = "http://localhost:4000/api/auth";
+const URL_ROOT = "https://dwfs16-m3s21-ejercicio-auth.onrender.com/api/auth";
 
 const loginService = async (form) => {
   const resp = await axios.post(`${URL_ROOT}/login`, form);
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const URL_ROOT = `http://localhost:4000/api/productos`;
+// const URL_ROOT = `http://localhost:4000/api/productos`;
+const URL_ROOT =
+  "https://dwfs16-m3s21-ejercicio-auth.onrender.com/api/productos";
 
 const getProductsService = async () => {
   const response = await axios.get(`${URL_ROOT}`);
